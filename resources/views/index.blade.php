b6cdc056766463cc93f1ca7c835ec28bee2cb2e4
Joe Dineen <jdineen21@gmail.com>
2019-10-02 12:49:23 +0100

    added meta description

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta name="Description" content="Portfolio Website for Joe Dineen (Web Developer)">
         <script src="js/app.js" defer></script>
         <script src="js/matrix.js" defer></script>
         <script src="js/modal.js" defer></script>
